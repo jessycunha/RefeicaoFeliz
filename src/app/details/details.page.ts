a8b6944d74c3be77afcc93d183301e48301bd291
Jessica Cunha <jessic4cunha@gmail.com>
2020-10-05 21:30:33 -0300

    end of page details

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
+  sliderConfig = {
+    slidesPerView: 2.5,
+    spaceBetween: 0,
+    initialSlide: 0,
+    speed: 400
+  }
+  
   constructor() { }
 
   ngOnInit() {
